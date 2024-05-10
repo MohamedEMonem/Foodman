50bd848da9c08646bf0dd11271412ebb469bc893
MohamedE <sim.mohamedessam44975@alexu.edu.eg>
2024-05-10 20:20:08 +0300

    refine

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
     require_once('config.php');
     
-    if(isset($_POST['submit'])){
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $name = $_POST['name'];
         $email = $_POST['email'];
         $message = $_POST['message'];
